1614d51d0eacac439dc6d70945c327a3c43f60f4
BingFengHung <joe94008@gmail.com>
2024-07-15 14:53:50 +0800

    <refactor>: 把 OutputDirectoryEntry 與 OutputFileEntry 兩個方法合併為一個

--- src/jtree/Program.cs
+++ src/jtree/Program.cs
@@ -1,7 +1,6 @@
 ﻿List<string> marks = ["├", "─", "└", "│"];
 
 var root =  Environment.CurrentDirectory;
-var entries =  Directory.EnumerateFileSystemEntries(root).ToList();
 
 bool IsDirectory(string path) 
 {
@@ -15,40 +14,36 @@ bool IsHidden(string path)
   return (attributes & FileAttributes.Hidden) == FileAttributes.Hidden;
 }
 
-void OutputDirectoryEntry(string entry, string indent, bool isLastEntry) {
-    var directory = new DirectoryInfo(entry);
-    Console.WriteLine(indent + (isLastEntry ? "└─" : "├─") + directory.Name);
+void OutputEntry(string entry, string indent, bool isLastEntry, bool isDirectory) {
+  var name = isDirectory ? new DirectoryInfo(entry).Name : new FileInfo(entry).Name; 
+  Console.WriteLine(indent + (isLastEntry ? "└─" : "├─") + name);
 }
 
-void OutputFileEntry(string entry, string indent, bool isLastEntry) {
-    var file = new FileInfo(entry);
-    Console.WriteLine(indent + (isLastEntry ? "└─" : "├─") + file.Name);
-}
+int deepLevel = 5;
 
-int deepLevel = 3;
-
-void Tree(List<string> entries, string indent, int level) {
+void Tree(IEnumerable<string> entries, string indent, int level) {
   if (deepLevel == level) return;
   
   var filteredEntries = entries.Where(e => !IsHidden(e)).ToList();
   
   int count = filteredEntries.Count;
   
-  for (var i = 0; i < count; i++) {
+  for (var i = 0; i < count; i++) 
+  {
     var entry = filteredEntries[i];
     bool isLastEntry = i == filteredEntries.Count - 1;
+    bool isDirectory = IsDirectory(entry);
+    
+    OutputEntry(entry, indent, isLastEntry, isDirectory);
 
     if (IsDirectory(entry)) {
-      OutputDirectoryEntry(entry, indent, isLastEntry);
       Tree(Directory.EnumerateFileSystemEntries(entry).ToList(), indent + (isLastEntry ? "    " : "│   "), level + 1);
-    } else {
-      OutputFileEntry(entry, indent, isLastEntry);
-    }
+    } 
   }
 }
 
-
-try { 
+void PrintRootName() 
+{
   if (IsDirectory(root)) {
     var dir = new DirectoryInfo(root);    
     Console.WriteLine(dir.Name);
@@ -56,6 +51,12 @@ try {
     var file = new FileInfo(root);    
     Console.WriteLine(file.Name);
   }
+}
+
+
+try { 
+  var entries =  Directory.EnumerateFileSystemEntries(root);
+  PrintRootName();
   Tree(entries, " ", 0);
 } catch(UnauthorizedAccessException ex) {
   Console.WriteLine(ex.Message);
