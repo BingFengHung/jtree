1f379846dc3518f0ba7c1025ec46c2e7086029e9
BingFengHung <joe94008@gmail.com>
2024-07-15 14:44:04 +0800

    <refactor>: 把資料夾與檔案的輸出另外再拉出來變成獨立的函式

--- src/jtree/Program.cs
+++ src/jtree/Program.cs
@@ -3,6 +3,28 @@
 var root =  Environment.CurrentDirectory;
 var entries =  Directory.EnumerateFileSystemEntries(root).ToList();
 
+bool IsDirectory(string path) 
+{
+  return Directory.Exists(path);
+}
+
+bool IsHidden(string path) 
+{
+  var attributes = File.GetAttributes(path);
+  
+  return (attributes & FileAttributes.Hidden) == FileAttributes.Hidden;
+}
+
+void OutputDirectoryEntry(string entry, string indent, bool isLastEntry) {
+    var directory = new DirectoryInfo(entry);
+    Console.WriteLine(indent + (isLastEntry ? "└─" : "├─") + directory.Name);
+}
+
+void OutputFileEntry(string entry, string indent, bool isLastEntry) {
+    var file = new FileInfo(entry);
+    Console.WriteLine(indent + (isLastEntry ? "└─" : "├─") + file.Name);
+}
+
 int deepLevel = 3;
 
 void Tree(List<string> entries, string indent, int level) {
@@ -17,27 +39,14 @@ void Tree(List<string> entries, string indent, int level) {
     bool isLastEntry = i == filteredEntries.Count - 1;
 
     if (IsDirectory(entry)) {
-      var directory = new DirectoryInfo(entry);
-      Console.WriteLine(indent +(isLastEntry ? "└─" : "├─") + directory.Name);
+      OutputDirectoryEntry(entry, indent, isLastEntry);
       Tree(Directory.EnumerateFileSystemEntries(entry).ToList(), indent + (isLastEntry ? "    " : "│   "), level + 1);
     } else {
-      var file = new FileInfo(entry);
-      Console.WriteLine(indent + (isLastEntry ? "└─" : "├─") + file.Name);
+      OutputFileEntry(entry, indent, isLastEntry);
     }
   }
 }
 
-bool IsDirectory(string path) 
-{
-  return Directory.Exists(path);
-}
-
-bool IsHidden(string path) 
-{
-  var attributes = File.GetAttributes(path);
-  
-  return (attributes & FileAttributes.Hidden) == FileAttributes.Hidden;
-}
 
 try { 
   if (IsDirectory(root)) {
